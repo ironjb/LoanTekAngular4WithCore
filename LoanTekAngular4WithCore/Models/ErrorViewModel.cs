827c95b5850600eced3a2dd7037418096ad7487e
ironjb <ironjb20@gmail.com>
2018-02-07 20:43:33 -0700

    Add project files.

---
+++ LoanTekAngular4WithCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LoanTekAngular4WithCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
